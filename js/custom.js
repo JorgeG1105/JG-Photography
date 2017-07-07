55a79555e298c39432457f4d551a078645d34871
Jorge Goris <J_goris@live.com>
2017-07-06 23:01:36 -0400

    Fixed some issues and Styled the Nav
    Also added some JS functionality.

---
+++ js/custom.js
@@ -0,0 +1,13 @@
+//When the page loads, Fade in all divs with class hidden.
+//Then Remove hidden class.
+$(document).ready(function () {
+    $('div.hidden').fadeIn(1000).removeClass('hidden');
+});
+
+//Fade In all h3 with class hidden.
+//Remove hidden class.
+$('h3.hidden').fadeIn(3000).removeClass('hidden');
+
+//When Top links are hovered
+//Slide the divs over a little bit.
+//On mouse off they go back to place
