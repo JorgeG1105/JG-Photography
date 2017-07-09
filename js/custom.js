d1c5045f793480fe4baf8198679402319e3a2843
Jorge Goris <J_goris@live.com>
2017-07-09 18:53:00 -0400

    Slight fixes

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
 //When the page loads, Fade in all divs with class hidden.
 //Then Remove hidden class.
 $(document).ready(function () {
-    $('div.hidden').fadeIn(1000).removeClass('hidden');
+    $('div.hidden').fadeIn(2000).removeClass('hidden');
 });
 
 //Fade In all h3 with class hidden.
@@ -11,3 +11,7 @@ $('h3.hidden').fadeIn(3000).removeClass('hidden');
 //When Top links are hovered
 //Slide the divs over a little bit.
 //On mouse off they go back to place
+
+
+//When bottom left arrow is clicked
+//Page scrolls up to the top.
